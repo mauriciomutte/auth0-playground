735882be3b1a68168ba02433b8c71a35011755ed
mauriciomutte <mauriciomutte.mm@gmail.com>
2025-01-12 15:59:34 -0300

    feat(scripts): add generate oauth client file script

---
+++ auth0-infra-as-code/scripts/generate-client-file.js
@@ -0,0 +1,26 @@
+const path = require("node:path");
+const fs = require("node:fs");
+
+const args = process.argv.slice(2);
+const [name, allowedLogoutUrls, allowedOrigins] = args;
+
+if (!name || !allowedLogoutUrls || !allowedOrigins) {
+    console.error('Usage: node generate-client-json.js <name> <allowed_logout_urls> <callbacks>');
+    process.exit(1);
+}
+
+const templatePath = path.join(__dirname, 'templates/client-template.json');
+const template = fs.readFileSync(templatePath, 'utf8');
+
+const logoutUrlArray = allowedLogoutUrls.split(',').map(url => `"${url.trim()}"`).join(', ');
+const callbackArray = allowedOrigins.split(',').map(url => `"${url.trim()}"`).join(', ');
+
+const output = template
+    .replace('"{{name}}"', `"${name}"`)
+    .replace('"{{allowed_logout_urls}}"', logoutUrlArray)
+    .replace('"{{callbacks}}"', callbackArray);
+
+const outputPath = `src/clients/${name}.json`;
+fs.writeFileSync(outputPath, output);
+
+console.log(`Client JSON file generated at ${outputPath}`);
